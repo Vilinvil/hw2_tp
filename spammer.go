b35e6beed2f8b1ad0386a9e059939177859bae0d
Vilinvil <ivn15072002@gmail.com>
2023-10-12 12:02:07 +0300

    first version of RunPipline

--- spammer.go
+++ spammer.go
@@ -1,7 +1,30 @@
 package main
 
-func RunPipeline(cmds ...cmd) {
+import "sync"
 
+func RunPipeline(cmds ...cmd) {
+	chIn := make(chan any)
+	chOut := make(chan any)
+	wg := &sync.WaitGroup{}
+
+	for _, cmdCur := range cmds {
+		wg.Add(1)
+
+		chInGo := chIn
+		chOutGo := chOut
+		cmdCur := cmdCur
+
+		go func() {
+			cmdCur(chInGo, chOutGo)
+			close(chOutGo)
+			wg.Done()
+		}()
+
+		chIn = chOutGo
+		chOut = make(chan any)
+	}
+
+	wg.Wait()
 }
 
 func SelectUsers(in, out chan interface{}) {
